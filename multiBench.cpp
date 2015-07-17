6ebbd42fcf96a7f89eba5af1b6be7892b2e531e7
Flugstein <flo.lugstein@gmail.com>
2015-07-17 11:12:00 +0200

    add cache line allociation, not implemented yet

--- multiBench.cpp
+++ multiBench.cpp
@@ -1,11 +1,15 @@
 #include <thread>
 #include <iostream>
 #include <chrono>
+#include "util.c"
 using namespace std;
 
 void benchFunc();
 
 int main(){
+
+    size_t cache_line_size= get_cache_line_size();
+    uint* cache_line= (uint*) malloc(cache_line_size);
 
     auto begin= chrono::high_resolution_clock::now();
 
@@ -24,7 +28,7 @@ int main(){
 }
 
 void benchFunc(){
-    thread_local int x;
+    thread_local uint x;
 
     for(thread_local uint32_t i; i < 1000; i++){
         x++;
